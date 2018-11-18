9a9314b9bf3f98abb79e3ee56e877789a5549e6f
wanghongkun <wanghongkun@corp.netease.com>
2018-11-18 17:06:17 +0800

    检测子元素是不是 button

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,7 +6,15 @@
 
 <script>
 export default {
-
+  mounted () {
+    console.log('here')
+    for(let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase()
+      if ( name !== 'button') {
+        console.warn(`gua-button-group 的子元素应该全是 gua-button, 但你写的是 ${name}`)
+      }
+    }
+  }
 }
 </script>
 
