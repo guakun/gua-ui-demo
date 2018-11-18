300906f2b6a6e8114da230c30b8ed30212029e40
wanghongkun <wanghongkun@corp.netease.com>
2018-11-18 17:01:36 +0800

    创建 button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('gua-button', Button)
 Vue.component('gua-icon', Icon)
+Vue.component('gua-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
