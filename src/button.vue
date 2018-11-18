13cb4399a23c7dd8aca2a3cd6fa54c26c48566be
wanghongkun <wanghongkun@corp.netease.com>
2018-11-18 16:06:56 +0800

    添加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,21 +1,27 @@
 <template>
-  <button class="gua-button">按钮</button>
+  <button class="gua-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
 <style lang="scss">
-.gua-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+.gua-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em; font-family: inherit;
+  border-radius: var(--border-radius); border: 1px solid var(--border-color); background: var(--button-bg);
+  display: inline-flex; justify-content: center; align-items: center; vertical-align: middle;
   &:hover { border-color: var(--border-color-hover); }
-
   &:active { background: var(--button-active-bg); }
-
   &:focus { outline: none; }
+  > .icon { order: 1; margin-right: .3em; }
+  > .content { order: 2; }
+  &.icon-right { >.content { order: 1; } > .icon { order: 2; margin-left: .3em; margin-right: 0; } }
 }
 </style>
