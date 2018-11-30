37370ca81f8ace95ff2abd96703e767868c274c3
wanghongkun <wanghongkun@corp.netease.com>
2018-11-30 15:21:16 +0800

    row 和 col 的雏形完成

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('gua-button', Button)
 Vue.component('gua-icon', Icon)
 Vue.component('gua-button-group', ButtonGroup)
 Vue.component('gua-input', Input)
+Vue.component('gua-row', Row)
+Vue.component('gua-col', Col)
 
 new Vue({
   el: '#app',
