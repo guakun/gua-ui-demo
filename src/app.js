4530bab013ab0ae4370de16d24df31e5a7003b81
wanghongkun <wanghongkun@corp.netease.com>
2018-12-03 15:12:22 +0800

    完成 toast 测试用例

--- src/app.js
+++ src/app.js
@@ -47,8 +47,7 @@ new Vue({
             console.log('他说他充值了')
           }
         },
-        autoClose: false,
-        autoCloseDelay: 3,
+        autoClose: false
       })
     }
   }
--- src/toast.vue
+++ src/toast.vue
@@ -25,12 +25,11 @@ export default {
   name: 'GuaToast',
   props: {
     autoClose: {
-      type: Boolean,
-      default: true
-    },
-    autoCloseDelay: {
-      type: Number,
-      default: 3
+      type: [Boolean, Number],
+      default: 5,
+      validator (value) {
+        return value === false || typeof value === 'number'
+      }
     },
     closeButton: {
       type: Object,
@@ -70,7 +69,7 @@ export default {
       if (this.autoClose) {
         setTimeout(() => {
           this.close()
-        }, this.autoCloseDelay * 1000)
+        }, this.autoClose * 1000)
       }
     },
     onClickClose () {
