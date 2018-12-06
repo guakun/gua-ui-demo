bfe734c44af8ca9b53066dff0bd5771694c6dfda
wanghongkun <wanghongkun@corp.netease.com>
2018-12-06 18:40:15 +0800

    添加 sass-loader 解决报错

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="button-demos">
+    <g-button>hi</g-button>  
+  </div>
+</template>
+
+<script>
+import Button from '../../../src/button'
+
+export default {
+  components: {
+    gButton: Button
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
