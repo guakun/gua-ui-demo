775fdb89e868e2f06e5b0a42fb4833a92940ae70
wanghongkun <wanghongkun@corp.netease.com>
2018-12-06 18:55:21 +0800

    创建官网的 button 部分

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="button-demos">
-    <g-button>hi</g-button>  
+    <g-button>默认按钮</g-button>  
+    <g-button icon="settings">默认按钮</g-button>  
+    <g-button icon="settings" loading>默认按钮</g-button>
+    <pre>
+      <code>{{content}}</code>
+    </pre>
   </div>
 </template>
 
@@ -10,6 +15,15 @@ import Button from '../../../src/button'
 export default {
   components: {
     gButton: Button
+  },
+  data () {
+    return {
+      content: `
+        <g-button>默认按钮</g-button>  
+        <g-button icon="settings">默认按钮</g-button>  
+        <g-button icon="settings" loading>默认按钮</g-button>
+      `
+    }
   }
 }
 </script>
