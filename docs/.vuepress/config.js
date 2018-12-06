9ed1361098ce8a270a6f064b480ee55a6404802c
wanghongkun <wanghongkun@corp.netease.com>
2018-12-06 18:14:39 +0800

    初始化 vuepress 配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'Gua UI',
+  description: '这条信息会展示在 meta 里面',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '文档', link: '/guide/' },
+      { text: '交流', link: 'https://google.com' },
+    ],
+    sidebar: [
+      {
+        title: '入门',
+        children: ['/install/', '/get-started/']
+      },
+      {
+        title: '组件',
+        children: ['/components/button']
+      }
+    ]
+  }
+}
