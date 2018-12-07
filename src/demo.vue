448cf1a81de0a12b692c6b3fb66a9872bf1a721b
wanghongkun <wanghongkun@corp.netease.com>
2018-12-07 10:09:00 +0800

    移除 eslint

---
+++ src/demo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>hi~
+        <g-button>hello</g-button>
+    </div>
+</template>
+
+<script>
+import GButton from './button'
+export default {
+    name: 'demo',
+    components: {
+        GButton
+    }
+}
+</script>
+
+<style>
+    * { margin: 0; padding: 0; box-sizing: border-box; }
+    body {
+      font-size: var(--font-size);
+      font-family: "Helvetica Neue",Helvetica,"Hiragino Sans GB","STHeitiSC-Light","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    }
+
+    html {
+      --font-size: 14px;
+    }
+</style>
