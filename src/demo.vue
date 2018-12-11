189b556019c764301acab2c46a649985c59985e4
wanghongkun <wanghongkun@corp.netease.com>
2018-12-11 16:45:22 +0800

    更新当前等级数据的时候删去后面的等级数据

--- src/demo.vue
+++ src/demo.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="demo">
-    <p>{{selected}}</p>
+    <p>{{selected && selected[0] && selected[0].name || '空' }}</p>
+    <p>{{selected && selected[1] && selected[1].name || '空' }}</p>
+    <p>{{selected && selected[2] && selected[2].name || '空' }}</p>
     <g-cascader :source="source" popover-height="170px" :selected.sync="selected"></g-cascader>
     <p>11111</p>
   </div>
