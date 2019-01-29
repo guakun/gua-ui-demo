6467ee44e103c048cdae3e3a5c6062485b2681ea
wanghongkun <wanghongkun@corp.netease.com>
2019-01-29 15:43:00 +0800

    添加一个 checkbox 组件;

---
+++ src/checkbox.vue
@@ -0,0 +1,39 @@
+<template>
+  <label class="gua-checkbox-wrapper">
+    <span class="gau-checkbox">
+      <input type="checkbox" class="gua-checkbox-input" ref="checkbox">
+      <span class="gua-checkbox-inner"></span>
+    </span>
+    <span>
+      <slot></slot>default
+    </span>
+  </label>
+</template>
+
+<script>
+  export default {
+    name: "GuaCheckbox",
+    props: {
+      indeterminate: {
+        type: Boolean,
+        default: false
+      }
+    },
+    watch: {
+      indeterminate(newVal) {
+        this.$refs.checkbox.indeterminate = newVal
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .gua-checkbox-wrapper {
+    border: 1px solid red;
+    cursor: pointer;
+    > span:last-child {
+      border: 1px solid red;
+      padding: 0 8px;
+    }
+  }
+</style>
